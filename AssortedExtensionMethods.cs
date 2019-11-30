4f4118fd3fa683a81b54d608b6757de046676f87
Jason T. Miller <jtm@jasomill.at>
2019-11-29 21:34:55 -0500

    Replace quick-and-dirty Quoted() extension  method with comprehensive JSON/C#-compatible implementation.

--- AssortedExtensionMethods.cs
+++ AssortedExtensionMethods.cs
@@ -1,8 +1,48 @@
 using System;
+using System.Text;
 
 namespace LagrangianDesign.LgTvControl.AssortedExtensions {
     public static class AssortedExtensionMethods {
-        public static String Quoted(this String s)
-            => s == null ? null : $"\"{s?.Replace("\\", "\\\\").Replace("\"", "\\\"")}\"";
+        public static String Quoted(this String s) {
+            if (s is null) {
+                return null;
+            }
+            var output = new StringBuilder(s.Length + 2);
+            output.Append('"');
+            foreach (var c in s) {
+                switch (c) {
+                    case '"':
+                        output.Append("\\\"");
+                        break;
+                    case '\\':
+                        output.Append("\\\\");
+                        break;
+                    case '\b':
+                        output.Append("\\b");
+                        break;
+                    case '\f':
+                        output.Append("\\f");
+                        break;
+                    case '\n':
+                        output.Append("\\n");
+                        break;
+                    case '\r':
+                        output.Append("\\r");
+                        break;
+                    case '\t':
+                        output.Append("\\t");
+                        break;
+                    default:
+                        if (c <= 0x1f) {
+                            output.AppendFormat("\\u{0:x4}", (Int16)c);
+                        } else {
+                            output.Append(c);
+                        }
+                        break;
+                }
+            }
+            output.Append('"');
+            return output.ToString();
+        }
     }
 }
